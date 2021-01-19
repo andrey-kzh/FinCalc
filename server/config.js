bfa657bdd7ecb1e5df9a69abaa3599c3a863276d
Quietmars <akpost@gmail.com>
2021-01-19 19:06:14 +0300

    new registration on server side and hash password

--- server/config.js
+++ server/config.js
@@ -6,5 +6,10 @@ module.exports = {
             pass: 'fIn60caLc629',
         }
     },
-    jwtSecretKey: 'jKie0S2j59lR5Hv30s3nhI84ka5br7'
+    jwtSecretKey: 'jKie0S2j59lR5Hv30s3nhI84ka5br7',
+    crypto: {
+        iterations: 12000,
+        length: 128,
+        digest: 'sha512',
+    }
 }
