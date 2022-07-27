a39341fb7e23ad55e691a66f71fd8eed65a66494
Quietmars <akpost@gmail.com>
2022-07-27 15:43:56 +0300

    demo preparation

--- server/controller/registration.js
+++ server/controller/registration.js
@@ -1,5 +1,6 @@
 const Users = require('../model/Users');
 
+// curl  -H 'Content-Type: application/json' --data '{"name":"Demo","login":"demo","password":"demo123"}' http://localhost:3000/api/registration
 module.exports.registration = async function registration(ctx, next) {
 
     if (!ctx.request.body.login) {
@@ -17,11 +18,11 @@ module.exports.registration = async function registration(ctx, next) {
     const name = ctx.request.body.name
 
     try {
-        const user = new Users({name: name, login: login});
+        const user = new Users({ name: name, login: login });
         await user.setPassword(password);
         await user.save();
         ctx.status = 200;
-        ctx.body = {userId: user._id};
+        ctx.body = { userId: user._id };
     } catch (err) {
         ctx.throw(400, err.message);
     }
